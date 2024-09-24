d26393eb567b15053c71938473d335f40e397fa5
krishkh <krishkhanna00@gmail.com>
2024-09-24 20:18:52 +0530

    added some color borders bla bla

--- src/app/layout.js
+++ src/app/layout.js
@@ -26,7 +26,7 @@ export default function RootLayout({ children }) {
       >
         {children}
         <DownNavbar />
-        <Footer />
+        {/* <Footer /> */}
       </body>
     </html>
   );
--- src/components/NameSection.jsx
+++ src/components/NameSection.jsx
@@ -1,4 +1,5 @@
 import ImageTooltip from "./ImageTooltip";
+import { BackgroundGradient } from "./ui/background-gradient";
 
 const NameSection = () => {
   return (
@@ -9,16 +10,21 @@ const NameSection = () => {
         </div>
         <div className="flex justify-center p-4 items-center text-white text-7xl font-lato font-thin">
           <ImageTooltip content="Handomse man">
+            {/* <BackgroundGradient className="rounded-full relative"> */}
+            {/* <div className="absolute w-[25rem] inset-0 bg-gradient-to-r from-purple-400 via-pink-500 to-red-500 rounded-full" /> */}
             <img
               src="/krish.jpg"
               alt="krish"
-              className="w-40 md:w-[25rem] rounded-full  aspect-[1] object-cover"
+              className="w-40 bg-gradient-to-r from-purple-400 via-pink-500 to-red-500 animate-gradient-move bg-[length:200%_200%]  border-4 md:w-[25rem] rounded-full  aspect-[1] p-2 object-cover"
             />
+            {/* </BackgroundGradient> */}
           </ImageTooltip>
         </div>
       </div>
       <div className="my-12 h-[2px] bg-gradient-to-r from-purple-400 via-pink-500 to-red-500 animate-gradient-move bg-[length:200%_200%]"></div>
-      {/* <div className="bg-black dark:bg-white my-12 p-[0.1px]"></div> */}
+      {/* <div className="m-12">
+        <BackgroundGradient className=" m-[0.1px]" />
+      </div> */}
     </div>
   );
 };
